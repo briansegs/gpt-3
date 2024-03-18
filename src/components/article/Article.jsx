41c4623b4c242dffc73ce3cdbfc80b3bea1a2275
bsegers <briansegs@gmail.com>
2024-03-18 11:12:47 -0400

    add: Article styles

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,15 +4,15 @@ import './article.css';
 const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='gpt3__blog-container_article'>
-        <div className="gpt3__blog-container-article-image">
+        <div className="gpt3__blog-container_article-image">
           <img src={imgUrl} alt="blog" />
         </div>
-        <div className="gpt3__blog-container-article-content">
+        <div className="gpt3__blog-container_article-content">
           <div>
             <p>{date}</p>
             <h3>{title}</h3>
-            <p>Read Full Article</p>
           </div>
+          <p>Read Full Article</p>
         </div>
     </div>
   )

73d6965e1016b9b0878588188575d8a554c90c07
bsegers <briansegs@gmail.com>
2024-03-19 11:34:33 -0400

    fix: Change dates

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -15,25 +15,25 @@ const Blog = () => {
           <div className="gpt3__blog-container_groupA">
             <Article 
               imgUrl={blog01} 
-              date='Sep 26, 2021'
+              date='Sep 26, 2024'
               title='GPT-3 and Open AI is the future. Let us explore how it is?' />
           </div>
           <div className="gpt3__blog-container_groupB">
             <Article 
               imgUrl={blog02} 
-              date='Sep 26, 2021'
+              date='Sep 26, 2024'
               title='GPT-3 and Open AI is the future. Let us explore how it is?' />
             <Article 
               imgUrl={blog03} 
-              date='Sep 26, 2021'
+              date='Sep 26, 2024'
               title='GPT-3 and Open AI is the future. Let us explore how it is?' />
             <Article 
               imgUrl={blog04} 
-              date='Sep 26, 2021'
+              date='Sep 26, 2024'
               title='GPT-3 and Open AI is the future. Let us explore how it is?' />
             <Article 
               imgUrl={blog05} 
-              date='Sep 26, 2021'
+              date='Sep 26, 2024'
               title='GPT-3 and Open AI is the future. Let us explore how it is?' />
           </div>
         </div>
